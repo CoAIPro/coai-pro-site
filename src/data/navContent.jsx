45bba77d47ba7becf0e49bea3b492dcc7b4d339b
Ahsan Raees <142185350+AhsanR-04@users.noreply.github.com>
2026-07-04 20:15:59 +0500

    Change Login and Signup links to external URLs
    Updated Login and Signup hrefs to point to external URLs.

--- src/data/navContent.jsx
+++ src/data/navContent.jsx
@@ -2,6 +2,6 @@ export const navItams = [
   { id: 1, label: "Home", href: "/" },
   { id: 2, label: "Dashboard", href: "/dashboard" },
   { id: 3, label: "Templates", href: "/templates" },
-  { id: 4, label: "Login", href: "/auth/login" },
-  { id: 5, label: "Signup", href: "/auth/signup" },
+  { id: 4, label: "Login", href: "https://app.coai.pro/sign-in" },
+  { id: 5, label: "Signup", href: "https://app.coai.pro/sign-up" },
 ]; 
